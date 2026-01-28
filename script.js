d71e582ecd7a9a7c9f9fc45ee51b3d4fd7759e91
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-28 20:16:07 +0800

    moved the hover effect from css to js because i will make it turn to randomized colors and add a progressive darkening effect in each interaction

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const container = document.querySelector("#container");
 
 grids = 16;
-numOfDivs = grids * grids;
-for (let i = 1; i <= numOfDivs; i++) {
+for (let i = 1; i <= grids * grids; i++) {
   const divGrid = document.createElement("div");
-  // divGrid.style.cssText = "width: 50px; height: 50px;";
+  divGrid.classList.add("divGrid");
   divGrid.style.cssText =
     "width: 50px; height: 50px; border: 1px solid black; box-sizing: border-box";
-  divGrid.classList.add("divGrid");
-  // div.textContent = `NO ${i}`;
-
+  divGrid.addEventListener(
+    "mouseover",
+    () => (divGrid.style.backgroundColor = "darkorchid"),
+  );
   container.appendChild(divGrid);
 }
 
 const cell = document.querySelector(".divGrid");
 
 const cellWidth = cell.offsetWidth;
-const cellHeight = cell.offsetHeight;
 
 container.style.width = `${cellWidth * grids}px`;
-container.style.height = `${cellHeight * grids}px`;
 
 console.log(`cellwidth: ${cellWidth}`);
 console.log(`cellheight: ${cellHeight}`);
-console.log(`numofDivs: ${numOfDivs}`);
 console.log(`container width: ${container.offsetWidth}`);
