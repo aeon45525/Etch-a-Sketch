ae4763bc0bd18975bc5981dc769b94294c0abcbc
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-28 18:57:30 +0800

    made 16x16 grid
    not dynamic, still needs manual computation to set size for the container depending on the height and width of the divs

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const container = document.querySelector("#container");
 numOfDivs = 16 * 16;
 for (let i = 1; i <= numOfDivs; i++) {
   const div = document.createElement("div");
-  div.style.cssText = "border: 2px solid black; width: 50px; height: 50px;";
-  div.textContent = `NO ${i}`;
+  div.style.cssText =
+    "width: 50px; height: 50px; border: 1px solid black; box-sizing: border-box";
+  // div.textContent = `NO ${i}`;
 
   container.appendChild(div);
 }
