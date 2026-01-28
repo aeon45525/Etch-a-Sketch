8bd55d9247dbb6f5cd2f15481632a83f7f2c9ddc
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-28 19:35:56 +0800

    calculation of CONTAINER SIZE now dynamic
    but still need to make the calculation of NUMBER OF GRIDS dynamic, it still depends on hard coded value

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container = document.querySelector("#container");
 
-numOfDivs = 16 * 16;
+grids = 16;
+numOfDivs = grids * grids;
 for (let i = 1; i <= numOfDivs; i++) {
   const divGrid = document.createElement("div");
+  // divGrid.style.cssText = "width: 50px; height: 50px;";
   divGrid.style.cssText =
     "width: 50px; height: 50px; border: 1px solid black; box-sizing: border-box";
   divGrid.classList.add("divGrid");
@@ -10,3 +12,16 @@ for (let i = 1; i <= numOfDivs; i++) {
 
   container.appendChild(divGrid);
 }
+
+const cell = document.querySelector(".divGrid");
+
+const cellWidth = cell.offsetWidth;
+const cellHeight = cell.offsetHeight;
+
+container.style.width = `${cellWidth * grids}px`;
+container.style.height = `${cellHeight * grids}px`;
+
+console.log(`cellwidth: ${cellWidth}`);
+console.log(`cellheight: ${cellHeight}`);
+console.log(`numofDivs: ${numOfDivs}`);
+console.log(`container width: ${container.offsetWidth}`);
