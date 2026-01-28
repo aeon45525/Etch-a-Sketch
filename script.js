5dc721102ddffbf5113ca6e6784eb2e62a352a8e
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-28 19:09:43 +0800

    added hover effect

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const container = document.querySelector("#container");
 
 numOfDivs = 16 * 16;
 for (let i = 1; i <= numOfDivs; i++) {
-  const div = document.createElement("div");
-  div.style.cssText =
+  const divGrid = document.createElement("div");
+  divGrid.style.cssText =
     "width: 50px; height: 50px; border: 1px solid black; box-sizing: border-box";
+  divGrid.classList.add("divGrid");
   // div.textContent = `NO ${i}`;
 
-  container.appendChild(div);
+  container.appendChild(divGrid);
 }
