59d5ff8ed823ee2be84881ce9190798d88616a54
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-30 18:48:31 +0800

    testing changes

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const container = document.querySelector("#container");
+function generateGrid(pix) {
+  const container = document.querySelector("#container");
 
-grids = 16;
-for (let i = 1; i <= grids * grids; i++) {
-  const divGrid = document.createElement("div");
-  divGrid.classList.add("divGrid");
-  divGrid.addEventListener(
-    "mouseover",
-    () => (divGrid.style.backgroundColor = "darkorchid"),
-  );
-  container.appendChild(divGrid);
+  // grids = 16;
+  for (let i = 1; i <= pix * pix; i++) {
+    const divGrid = document.createElement("div");
+    divGrid.classList.add("divGrid");
+    divGrid.addEventListener(
+      "mouseover",
+      () => (divGrid.style.backgroundColor = "darkorchid"),
+    );
+    container.appendChild(divGrid);
+  }
+
+  const cell = document.querySelector(".divGrid");
+  const cellWidth = cell.offsetWidth;
+  container.style.width = `${cellWidth * pix}px`;
 }
 
-const cell = document.querySelector(".divGrid");
+const generateBtn = document.querySelector("#generateBtn");
+generateBtn.addEventListener("click", userInput);
 
-const cellWidth = cell.offsetWidth;
-
-container.style.width = `${cellWidth * grids}px`;
+function userInput() {
+  let pixels = prompt("Please enter number of squares per side for the grid: ");
+  if (pixels > 100) {
+    alert("Maximun input of 100 grids!");
+  } else {
+    generateGrid(pixels);
+  }
+}
 
 // console.log(`cellwidth: ${cellWidth}`);
 // console.log(`cellheight: ${cellHeight}`);
